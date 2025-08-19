a75fed61a49bf92d7017452ce9b068b1f4672c45
Michael <michael.ispas@gmail.com>
2025-08-19 10:19:47 -0700

    fix: simplifed logic, now functional.

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ class TranscribeApp: # Covers GUI basic features
 
     @staticmethod
     def run_transcription(filepath, model_size, device, compute_type):
-        from transcriber import Transcriber
         transcriber = Transcriber(model_size=model_size, device=device, compute_type=
                                   compute_type)
         transcriber.transcribe(filepath)
@@ -51,7 +50,6 @@ class TranscribeApp: # Covers GUI basic features
             self.filepath = filepath
             print(f"Selected file: {filepath}")
             self.open_new_window()
-            self.transcribe_button.config(state=tk.NORMAL)
         else:
             self.filepath = None
             self.transcribe_button.config(state=tk.DISABLED)
@@ -62,12 +60,12 @@ class TranscribeApp: # Covers GUI basic features
         self.new_window.geometry("250x150")
         self.transcribe_button = tk.Button(self.new_window, text="Transcribe",
                                         command=self.transcribe_file,
-                                        state=tk.DISABLED)
+                                        state=tk.NORMAL if self.filepath else
+                                        tk.DISABLED)
         self.transcribe_button.pack(pady=10)
 
     def transcribe_file(self):
         try:
-            self.transcribe_button.config(state=tk.DISABLED)
             self.transcribe_button.config(text="Cancel", state=tk.NORMAL,
                                           command=self.cancel)
             self.transcription_process = Process(
@@ -77,14 +75,10 @@ class TranscribeApp: # Covers GUI basic features
             #messagebox.showinfo(message="Transcription Completed.")
         except Exception as e:
             messagebox.showerror(message=f"Error, Transcription failed: {str (e)}.")
-        finally:
-            self.transcribe_button.config(text="Transcribe", state=tk.NORMAL)
 
     def cancel(self):
-        if hasattr(self, 'transcription_process') and self.transcription_process and self.  transcription_process.is_alive():
-            self.transcription_process.terminate()
-        if hasattr(self, 'new_window') and self.new_window.winfo_exists():
-            self.new_window.destroy()
+        self.transcription_process.terminate()
+        self.new_window.destroy()
 
 if __name__ == "__main__":
     root = tk.Tk()
