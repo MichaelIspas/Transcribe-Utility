e086403ad98b2f8a794093d821f5092e08472061
Michael <michael.ispas@gmail.com>
2025-11-12 13:05:52 -0800

    chore: added clarifying comments

--- srt_handler.py
+++ srt_handler.py
@@ -7,5 +7,15 @@ from datetime import timedelta
 class SRTHandler:
     """Handles generation and saving of SRT subtitle files from transcription data."""
 
-    def convert_time(seconds=):
+    # example output from text file before srt conversion
+    # 0.00s -> 6.80s]  Hi guys, so it's been quite a while.
 
+    # example output after srt conversion
+    # 1
+    # 00:00:00,000 --> 00:00:06,800
+    # Hi guys, so it's been quite a while.
+    # 
+    # 2
+    # 00:00...
+
+    def convert_time():
